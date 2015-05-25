2a3be4a1a5d91e1934785c5f15a29f71403ce221
lyjloveabc <546223592@qq.com>
2015-05-25 13:39:07 +0800

    Signed-off-by: lyjloveabc <546223592@qq.com>

---
+++ perl/ex/pl/getPage.pl
@@ -0,0 +1,22 @@
+#!perl -w
+# file get_url.pl
+#--开启语法检查并使用LWP modules
+use strict;
+use LWP;
+
+#--取得URL
+my $url = "http://cring.118320.com:8080/zjwap/rings/indexS.action";
+
+#--建立LWP ::UserAgent 与HTTP ::Request 物件，
+#--其中Request对象将$url传进去
+my $agent = LWP::UserAgent->new();
+my $request = HTTP::Request->new( GET => $url );
+
+#--透过UserAgent的request method将Request送出
+my $response = $agent->request($request);
+
+#--检查是否有error发生
+$response->is_success or die "$url: ", $response->message, "/n";
+
+#--显示responser的内容
+print $response->content;
--- perl/ex/pl/test.pl
+++
@@ -1,8 +0,0 @@
-#!perl -w
-use strict;
-use LWP;
-
-my $browser = LWP::UserAgent->new();
-my $response= $browser->get("http://www.toolmao.com");
-
-print $response->content; # 输出获得的网页内容
